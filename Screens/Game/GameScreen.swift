77fb3ee2068151334fe3ca95dcee18edda3b141c
JNox <32821440+j-nox@users.noreply.github.com>
2022-11-05 18:28:55 +0300

    Game results screen (#6)

--- Screens/Game/GameScreen.swift
+++ Screens/Game/GameScreen.swift
@@ -5,22 +5,31 @@ struct GameScreen: View {
   @StateObject var viewModel: GameScreenViewModel = GameScreenViewModel()
   
   var body: some View {
-    HStack {
-      Text("\(viewModel.message)")
-      Button {
-        if viewModel.gameIsRunning {
-          viewModel.step()
-        } else {
-          viewModel.startGame()
-        }
-      } label: {
-        if viewModel.gameIsRunning {
-          Text("Next step")
-        } else {
-          Text("Start game")
+    VStack {
+      HStack {
+        Text("Step \(viewModel.currentPlayerName)")
+      }
+      HStack {
+        Text("\(viewModel.message)")
+        Button {
+          if viewModel.gameIsRunning {
+            viewModel.step()
+          } else {
+            viewModel.startGame()
+          }
+        } label: {
+          if viewModel.gameIsRunning {
+            Text("Next step")
+          } else {
+            Text("Start game")
+          }
         }
       }
-      
+      .sheet(isPresented: $viewModel.showResult, onDismiss: {
+        viewModel.showResult = false
+      }) {
+        GameResultsScreen(stepCount: viewModel.stepCount, mysteriousNumber: viewModel.mysteriousNumber)
+      }
     }
   }
 }
--- Screens/GameResults/GameResultsScreen.swift
+++ Screens/GameResults/GameResultsScreen.swift
@@ -1,7 +1,16 @@
 import SwiftUI
 
 struct GameResultsScreen: View {
+  
+    @State var stepCount = 0
+  
+    @State var mysteriousNumber = 0
+  
+    @State var winner = ""
+  
     var body: some View {
-        Text("Game ended")
+      Text("Game ended")
+      Text("Steps count: \(stepCount)")
+      Text("Mysterious integer: \(mysteriousNumber)")
     }
 }
